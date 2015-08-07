3b934a1310637048d84322397123c5b39afc9d65
alex-wilmer <djazium@gmail.com>
2015-08-06 23:06:39 -0400

    gulp

--- dest/public/js/components.js
+++ dest/public/js/components.js
@@ -1,23 +1,51 @@
 'use strict';
 
 (function () {
+  var TxList = React.createClass({
+    displayName: 'TxList',
+
+    render: function render() {
+      var latestTransaction = this.props.data;
+      return React.createElement(
+        'ul',
+        null,
+        'hey ',
+        latestTransaction
+      );
+    }
+  });
+
   var TxFeed = React.createClass({
     displayName: 'TxFeed',
 
-    render: function render() {
-      var latestTransaction = this.props.data.id;
+    getInitialState: function getInitialState() {
+      return { data: [] };
+    },
 
+    componentDidMount: function componentDidMount() {
+      var self = this;
+
+      socket.on('new_transaction', function (response) {
+        var data = self.state.data.unshift(response).slice(0, 9);
+        self.setState({ data: data });
+      });
+    },
+
+    render: function render() {
       return React.createElement(
-        'h1',
+        'div',
         null,
-        'latest transactions'
+        React.createElement(
+          'h1',
+          null,
+          'latest transactions'
+        ),
+        React.createElement(TxList, { data: this.state.data })
       );
     }
   });
 
   var socket = io.connect(location.href);
 
-  socket.on('new_transaction', function (data) {
-    React.render(React.createElement(TxFeed, { data: data }), document.getElementById('mount-point'));
-  });
+  React.render(React.createElement(TxFeed, null), document.getElementById('mount-point'));
 })();
